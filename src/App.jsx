5559467941edb0d67053f0ae5e2f93f7ae02761b
oninotna <antoninomusarratubbi@gmail.com>
2025-05-26 19:56:47 +0200

    add delete button

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,19 @@ export default function App() {
     const newArticle = {
       id: Date.now(),
       title: title
-    }
+    };
 
-console.log(newArticle);
+    console.log(newArticle);
+    console.log(defaultArticles);
     setArticles([...articles, newArticle]);
+    
     setTitle('');
-  }
+  };
+
+  const handleClickDelete = (idToDelete) => {
+    const newArticles = articles.filter(article => article.id != idToDelete);
+    setArticles(newArticles);
+  };
 
 
   return (
@@ -28,13 +35,15 @@ console.log(newArticle);
         <input 
           type="text" 
           value={title}
-          onChange={ (e) => {setTitle(e.target.value)}} />
+          onChange={(e) => {setTitle(e.target.value)}} />
         <button className="btn btn-primary ms-2" type="submit">Submit form</button>
       </form>
 
-      <ul className="mx-auto" style={{maxWidth:'120px'}}>
+      <ul className="mx-auto" style={{maxWidth:'200px'}}>
         {articles.map(article => 
-          <li key={article.id}>{article.title}</li>
+          <li className="mb-2 d-flex justify-content-between align-items-center" key={article.id}>{article.title} 
+            <button onClick={() => handleClickDelete(article.id)} className="btn btn-primary ms-2">delete</button>
+          </li>            
         )}
       </ul>
     </div>
